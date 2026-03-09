3e253bd5de670df4411978c3a8f6a13d7d7cf871
Wren <grahamgrasley@gmail.com>
2026-03-09 15:40:48 -0400

    island buildingize prototype

---
+++ Buildings/Mailbox.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Buildings;
+using System.Xml.Serialization;
+
+namespace FunkyBuildings.Buildings;
+
+[XmlType("Mods_" + MOD_ID + "_Mailbox")]
+public class Mailbox : Building
+{
+	public override void draw(SpriteBatch b)
+	{
+		base.draw(b);
+
+		if (Game1.mailbox.Count is 0)
+			return;
+
+		float draw_layer = (tileX.Value + 1) * 64 / 10000f + tileY.Value * 64 / 10000f;
+		float yOffset = 4f * (float)Math.Round(Math.Sin(Game1.currentGameTime.TotalGameTime.TotalMilliseconds / 250.0), 2);
+
+		b.Draw(
+			Game1.mouseCursors, 
+			Game1.GlobalToLocal(Game1.viewport, new Vector2(tileX.Value * 64, tileY.Value * 64 - 96 - 48 + yOffset)), 
+			new Rectangle(141, 465, 20, 24), 
+			Color.White * 0.75f, 
+			0f, 
+			Vector2.Zero, 
+			4f, 
+			SpriteEffects.None, 
+			draw_layer + 1E-06f
+		);
+		b.Draw(
+			Game1.mouseCursors, 
+			Game1.GlobalToLocal(Game1.viewport, new Vector2(tileX.Value * 64 + 32 + 4, tileY.Value * 64 - 64 - 24 - 8 + yOffset)),
+			new Rectangle(189, 423, 15, 13), 
+			Color.White, 
+			0f, 
+			new Vector2(7f, 6f), 
+			4f, 
+			SpriteEffects.None, 
+			draw_layer + 1E-05f
+		);
+	}
+}
---
+++ Buildings/VanillaIslandBuildings.cs
@@ -0,0 +1,90 @@
+﻿using FunkyBuildings.Framework;
+using Microsoft.Xna.Framework;
+using Netcode;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Locations;
+using StarModGen.Lib;
+using xTile;
+using xTile.Layers;
+
+namespace FunkyBuildings.Buildings;
+
+internal class VanillaIslandBuildings
+{
+	private static IModHelper helper = null!;
+
+	[ModEvent]
+	public static void Init(object? s, SetupEventArgs ev)
+	{
+		helper = ev.Helper;
+
+		ev.Harmony
+			.With<IslandWest>(nameof(IslandWest.ApplyFarmHouseRestore)).Postfix(ApplyFarmhouse)
+			.With(nameof(IslandWest.ApplyFarmObeliskBuild)).Postfix(ApplyObelisk);
+	}
+
+	private static void ApplyFarmhouse(IslandWest __instance, NetHashSet<string> ____appliedMapOverrides)
+	{
+		if (____appliedMapOverrides.Contains("Island_House_Restored"))
+		{
+			DemolishMapBuilding(new(74, 37, 7, 5), 9, __instance);
+			DemolishMapBuilding(new(__instance.shippingBinPosition, new(2, 1)), 2, __instance);
+			__instance.AddDefaultBuilding(MOD_ID + "_IslandFarmhouse", new(74, 37));
+		}
+
+		if (!__instance.modData.ContainsKey(MOD_ID + "_AddedShippingBin"))
+		{
+			__instance.modData[MOD_ID + "_AddedShippingBin"] = "T";
+			__instance.AddDefaultBuilding("ShippingBin", __instance.shippingBinPosition.ToVector2());
+		}
+
+		if (__instance.farmhouseMailbox.Value)
+		{
+			DemolishMapBuilding(new(81, 40, 1, 1), 2, __instance);
+			__instance.AddDefaultBuilding(MOD_ID + "_Mailbox", new(81, 40));
+		}
+	}
+
+	private static void ApplyObelisk(IslandWest __instance, NetHashSet<string> ____appliedMapOverrides)
+	{
+		if (____appliedMapOverrides.Contains("Island_W_Obelisk"))
+		{
+			DemolishMapBuilding(new(71, 36, 3, 2), 9, __instance);
+			__instance.AddDefaultBuilding(MOD_ID + "_IslandFarmObelisk", new(71, 36));
+		}
+	}
+
+	private static void DemolishMapBuilding(Rectangle foot, int height, GameLocation where)
+	{
+		if (where?.Map is not Map map)
+			return;
+
+		for (int y = 0; y < height; y++)
+		{
+			int tileY = foot.Bottom - y - 1;
+			string layerName = y < foot.Height ? "Buildings" : y > foot.Height ? "AlwaysFront" : "Front";
+			EraseLine(map.GetLayer(layerName), foot.X, tileY, foot.Width);
+		}
+	}
+
+	private static void EraseLine(Layer layer, int x, int y, int width)
+	{
+		for (int i = 0; i < width; i++)
+			layer.Tiles[x + i, y] = null;
+	}
+
+	[ModEvent]
+	internal static void HandleAsset(object? s, AssetRequestedEventArgs e)
+	{
+		if (e.NameWithoutLocale.IsEquivalentTo("Maps/Island_W"))
+			e.Edit(EditIslandFarm, AssetEditPriority.Late);
+	}
+
+	private static void EditIslandFarm(IAssetData data)
+	{
+		var map = data.AsMap();
+		map.PatchMap(helper.ModContent.Load<Map>("assets/maps/island_house_patch.tmx"), targetArea: new(80, 41, 7, 2), patchMode: PatchMapMode.ReplaceByLayer);
+	}
+}
